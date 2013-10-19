39e052863a4b03ba52591393b12123496d1c4d06
Krishna Ram Prakash <krishnaramprakash@gmail.com>
2013-10-20 02:54:35 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  untitled.py
+#  
+#  Copyright 2013 Krishna Ram <geekytux@fox>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+import requests
+from BeautifulSoup import BeautifulSoup
+
+def fetchDoc(url):
+	r=requests.get(url);
+	if r.status_code != 200:
+		print "Cannot fetch Document!"
+		exit;
+	return r.text
+
+def main():
+	target="http://localhost/projects/"
+	doc=fetchDoc(target)
+	pDoc=BeautifulSoup(doc)
+	url=[]
+	for link in pDoc.html.body.table.findAll("a"):
+		url.append(target+link.get('href'));
+	completeUrl=url[5:len(url):1]
+	print completeUrl
+
+if __name__ == '__main__':
+	main()
+	
