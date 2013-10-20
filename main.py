cea9b29b851786147d54762974010da6afc90378
Krishna Ram Prakash <krishnaramprakash@gmail.com>
2013-10-20 18:29:44 +0530

    Listing files and folders recursively

--- main.py
+++ main.py
@@ -28,18 +28,36 @@ from BeautifulSoup import BeautifulSoup
 def startGrabbing(target):
 	doc=fetchDoc(target)
 	pDoc=BeautifulSoup(doc)
+	#Checking if it is a directory listing
+	try:
+		if pDoc.head.title.text[:8] != ("Index of"):
+			return 
+	except:
+		return
 	dirList=[]
 	fileList=[]
-	for link in pDoc.html.body.table.findAll("a"):
-		if isValidUrl(link.get('href'))==False:
-			continue;
-		compUrl=target+link.get('href')
-		if compUrl[-1:]=='/': #Checking if it is directory
-			dirList.append(compUrl)
-		else:
-			fileList.append(compUrl)
-	print fileList
-	print dirList
+	
+	try:
+		for link in pDoc.html.body.table.findAll("a"):
+			#Checking whether URL is valid
+			if isValidUrl(link.get('href'))==False:
+				continue;
+			
+			urlHref=link.get('href')
+			
+			if urlHref[-1:]=='/': #Checking if it is directory
+				dirList.append(target+urlHref)
+			else:
+				fileList.append(target+urlHref)
+	except:
+		return
+	#output
+	printList(target,[],fileList)
+	
+	#Grabbing the subdirs		
+	for subDir in dirList:
+		startGrabbing(subDir)
+		#~ print target+subDir
 
 def fetchDoc(url):
 	r=requests.get(url);
@@ -49,13 +67,24 @@ def fetchDoc(url):
 	return r.text
 
 def isValidUrl(url):
-	if url[:1]=='?' or url=='/':
+	if url[:1]=='?' or url[:1]=='/':
 		return False
 	else:
 		return True;
+		
+def printList(target,dirList,fileList):
+	if len(fileList)!=0:
+		#~ print target+" - Files:"
+		for filename in fileList:
+			print filename
+	
+	if len(dirList)!=0:
+		#~ print target+" - Folders:"
+		for dirName in dirList:
+			print dirName
 
 def main():
-	target="http://localhost/projects/"
+	target="http://www.kernel.org/pub/"
 	startGrabbing(target)
 	
 	
