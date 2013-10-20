1afa5f179d8504b49f9fd22b9d6be406bb2ad169
Krishna Ram Prakash <krishnaramprakash@gmail.com>
2013-10-20 17:31:38 +0530

    Readiying recursive function

--- main.py
+++ main.py
@@ -25,6 +25,22 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 
+def startGrabbing(target):
+	doc=fetchDoc(target)
+	pDoc=BeautifulSoup(doc)
+	dirList=[]
+	fileList=[]
+	for link in pDoc.html.body.table.findAll("a"):
+		if isValidUrl(link.get('href'))==False:
+			continue;
+		compUrl=target+link.get('href')
+		if compUrl[-1:]=='/': #Checking if it is directory
+			dirList.append(compUrl)
+		else:
+			fileList.append(compUrl)
+	print fileList
+	print dirList
+
 def fetchDoc(url):
 	r=requests.get(url);
 	if r.status_code != 200:
@@ -40,20 +56,7 @@ def isValidUrl(url):
 
 def main():
 	target="http://localhost/projects/"
-	doc=fetchDoc(target)
-	pDoc=BeautifulSoup(doc)
-	dirList=[]
-	fileList=[]
-	for link in pDoc.html.body.table.findAll("a"):
-		if isValidUrl(link.get('href'))==False:
-			continue;
-		compUrl=target+link.get('href')
-		if compUrl[-1:]=='/':
-			dirList.append(compUrl)
-		else:
-			fileList.append(compUrl)
-	print fileList
-	print dirList
+	startGrabbing(target)
 	
 	
 if __name__ == '__main__':
